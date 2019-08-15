c1e7f04409d1f74fdb0f28265b2dff16c16779d0
vjoensen <vjo@kapacity.dk>
2019-08-15 16:38:39 +0200

    Add project files.

---
+++ A5k/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace A5k
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new MainWindow().Run(60);
+        }
+    }
+}
