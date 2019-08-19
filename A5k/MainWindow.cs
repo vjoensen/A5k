2bbb86b3c9ae8854a60446612a57e3702ac85cc6
Viktor Joensen <viktor.joensen@gmail.com>
2019-08-19 20:10:39 +0200

    Check-in before the storm

--- A5k/MainWindow.cs
+++ A5k/MainWindow.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+//using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,12 +18,15 @@ namespace A5k
     {
         private readonly string _title;
         private int _program;
+        private int _program2;
         private int _vertexArray;
         private double _time;
+        private int texture;
+        uint VBO, VAO, EBO;
 
         public MainWindow()
-            : base(1280, // initial width
-                720, // initial height
+            : base(640, // initial width
+                480, // initial height
                 GraphicsMode.Default,
                 "A5k",  // initial title
                 GameWindowFlags.Default,
@@ -41,12 +46,67 @@ namespace A5k
         protected override void OnLoad(EventArgs e)
         {
             CursorVisible = true;
+            //GL.Disable()
+
 
             _program = CreateProgram();
-            GL.GenVertexArrays(1, out _vertexArray);
-            GL.BindVertexArray(_vertexArray);
-            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
-            GL.PatchParameter(PatchParameterInt.PatchVertices, 3);
+
+            //GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+            //
+            //
+            //GL.PatchParameter(PatchParameterInt.PatchVertices, 6);
+            //GL.GenVertexArrays(1, out _vertexArray);
+            float[] vertices = {
+                 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+                 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+                -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+                -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+            };
+
+            uint[] indices = {
+                0, 1, 3, // first triangle
+                1, 2, 3  // second triangle
+            };
+
+
+            //int VBO, VAO, EBO;
+            GL.GenVertexArrays(1, out VAO);
+            GL.GenBuffers(1, out VBO);
+            GL.GenBuffers(1, out EBO);
+
+            GL.BindVertexArray(VAO);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length, vertices, BufferUsageHint.StaticDraw);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, EBO);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length, indices, BufferUsageHint.StaticDraw);
+
+
+            // position attribute
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            // color attribute
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), (3 * sizeof(float)));
+            GL.EnableVertexAttribArray(1);
+            // texture coord attribute
+            GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), (6 * sizeof(float)));
+            GL.EnableVertexAttribArray(2);
+
+            texture = LoadTexture("PNG\\playerShip1_red.png");
+
+            GL.UseProgram(_program);
+
+            GL.Uniform1(GL.GetUniformLocation(_program, "ourTexture"), 0);
+
+            //System.Diagnostics.Debug.WriteLine(GL.GetUniformLocation(_program, "texture1"));
+
+
+           // GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), (6 * sizeof(float)));
+           // GL.EnableVertexAttribArray(2);
+           //
+           // GL.BindVertexArray(_vertexArray);
+
             Closed += OnClosed;
         }
         private void OnClosed(object sender, EventArgs eventArgs)
@@ -62,14 +122,48 @@ namespace A5k
             base.Exit();
         }
 
+        public int LoadTexture(string file)
+        {
+            Bitmap bitmap = new Bitmap(file);
+
+            int tex;
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+            GL.GenTextures(1, out tex);
+            GL.BindTexture(TextureTarget.Texture2D, tex);
+
+            System.Drawing.Imaging.BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            bitmap.UnlockBits(data);
+
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            return tex;
+        }
+
+
+        public Bitmap loadBitmap(string filepath)
+        {
+            return new Bitmap("PNG\\playerShip1_red.png");
+        }
+
         private int CreateProgram()
         {
             try
             {
                 var program = GL.CreateProgram();
                 var shaders = new List<int>();
-                shaders.Add(CompileShader(ShaderType.VertexShader, @"Shaders\1Vert\vertexShader.c"));
-                shaders.Add(CompileShader(ShaderType.FragmentShader, @"Shaders\5Frag\fragmentShader.c"));
+                shaders.Add(CompileShader(ShaderType.VertexShader, @"Shaders\1Vert\vertexShaderTex.c"));
+                shaders.Add(CompileShader(ShaderType.FragmentShader, @"Shaders\5Frag\fragmentShaderTex.c"));
 
                 foreach (var shader in shaders)
                     GL.AttachShader(program, shader);
@@ -80,7 +174,7 @@ namespace A5k
 
                 foreach (var shader in shaders)
                 {
-                    GL.DetachShader(program, shader);
+                    //GL.DetachShader(program, shader);
                     GL.DeleteShader(shader);
                 }
                 return program;
@@ -130,21 +224,29 @@ namespace A5k
             GL.ClearColor(backColor);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, texture);
+
+
             GL.UseProgram(_program);
 
+            GL.BindVertexArray(VAO);
+            GL.DrawElements(BeginMode.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+
             // add shader attributes here
-            GL.VertexAttrib1(0, _time);
-            Vector4 position;
-            position.X = (float)Math.Sin(_time) * 0.5f;
-            position.Y = (float)Math.Cos(_time) * 0.5f;
-            position.Z = 0.0f;
-            position.W = 1.0f;
-            GL.VertexAttrib4(1, position);
-
-
-
-            GL.DrawArrays(PrimitiveType.Points, 0, 1);
-            GL.PointSize(100);
+            //  GL.VertexAttrib1(0, _time);
+            //  
+            //  Vector4 position;
+            //  position.X = (float)Math.Sin(_time) * 0.5f;
+            //  position.Y = (float)Math.Cos(_time) * 0.5f;
+            //  position.Z = 0.0f;
+            //  position.W = 1.0f;
+            //  GL.VertexAttrib4(1, position);
+            //  
+            //  
+            //  
+            //  GL.DrawArrays(PrimitiveType.Points, 0, 1);
+            //  GL.PointSize(100);
             SwapBuffers();
         }
     }
